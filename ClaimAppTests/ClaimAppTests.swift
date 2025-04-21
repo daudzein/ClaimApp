993eff4aa88df469f08bd9d542ebe685881f3c2c
Daud Zein <daudzein@Gmail.com>
2025-04-21 15:27:53 +0700

    Commit unit testing

---
+++ ClaimAppTests/ClaimAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClaimAppTests.swift
+//  ClaimAppTests
+//
+//  Created by daud daud on 21/04/25.
+//
+
+import Testing
+
+struct ClaimAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
